d6b1483dc4f382e92ad9fc5e400f65884830c0ca
John Fernkas <john@johnfernkas.com>
2020-04-11 23:26:42 -0400

    Update install.sh to loop through dotfiles when setting up symlinks

--- install.sh
+++ install.sh
@@ -2,25 +2,30 @@
 
 echo "Setting up your Mac..."
 
-# Check for Homebrew and install it if we don't have it
-if test ! $(which brew); then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+# # Check for Homebrew and install it if we don't have it
+# if test ! $(which brew); then
+#     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# fi
 
-# Update Homebrew recipes
-brew update
+# # Update Homebrew recipes
+# brew update
 
-# Install Homebrew dependencies with bundle
-brew tap homebrew/bundle
-brew bundle
+# # Install Homebrew dependencies with bundle
+# brew tap homebrew/bundle
+# brew bundle
 
-# Remove .zshrc from $HOME and symlinks it from the .dotfiles directory
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+# Remove dotfiles locally and setup symlinks
+DIR=$HOME/.dotfiles
 
-# Remove .gitconfig from $HOME and symlinks it from the .dotfiles directory
-rm -rf $HOME/.gitconfig
-ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+DOTFILES=(
+    ".zshrc"
+    ".gitconfig"
+)
 
-# Symlink settings.json
+for dotfile in "${DOTFILES[@]}";do
+	rm -rf "${HOME}/${dotfile}"
+	ln -sf "${DIR}/${dotfile}" "${HOME}/${dotfile}"
+done
+
+# Special case symlink for settings.json becuase of the local file path
 ln -s $HOME/.dotfiles/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
