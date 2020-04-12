cf3b807a0408362f368cdf6795207adc6b8afa82
John Fernkas <john@johnfernkas.com>
2020-04-11 22:07:55 -0400

    Add install.sh to install homebrew and setup symlinks

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install it if we don't have it
+if test ! $(which brew); then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install Homebrew dependencies with bundle
+brew tap homebrew/bundle
+brew bundle
+
+# Remove .zshrc from $HOME and symlinks it from the .dotfiles directory
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Remove .gitconfig from $HOME and symlinks it from the .dotfiles directory
+rm -rf $HOME/.gitconfig
+ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
